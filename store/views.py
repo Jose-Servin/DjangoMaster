bfb6c714d9c093ea5091f5bdbfd9a624b5662fea
Jose Servin <servinjose96@gmail.com>
2024-05-28 18:00:11 -0500

    how to Update products

--- store/views.py
+++ store/views.py
@@ -6,26 +6,38 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
-@api_view(['GET', 'POST'])
+@api_view(["GET", "POST"])
 def product_list(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         query_set = Product.objects.select_related("collection").all()
         serializer = ProductSerializer(
-            query_set, many=True, context={"request": request})
+            query_set, many=True, context={"request": request}
+        )
         return Response(serializer.data)
-    elif request.method == 'POST':
+    elif request.method == "POST":
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.validated_data
-        return Response('ok')
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view()
+@api_view(["GET", "PUT"])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product)
-
-    return Response(serializer.data)
+    if request.method == "GET":
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        # de-serialize incoming data and provide the product instance we are working on
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        """
+        when we call the serializer.save() method,
+        the serializer will call the update() method
+        because we are instantiating it with a product instance
+        """
+        serializer.save()
+        return Response(serializer.data)
 
 
 @api_view()
