9b43f7e66d1be8fc9096cf5357f199055b465da0
Jose Servin <servinjose96@gmail.com>
2024-06-13 20:03:04 -0500

    add Product and Collection view set views

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework import status
@@ -10,17 +11,12 @@ from .models import Product, Collection
 from .serializers import CollectionSerializer, ProductSerializer
 
 
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related("collection").all()
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     def get_serializer_context(self):
         return {"request": self.request}
-
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
 
     def delete(self, request, *args, **kwargs):
         product = self.get_object()
@@ -29,23 +25,18 @@ class ProductDetail(RetrieveUpdateDestroyAPIView):
                 {
                     "error": "Product cannot be deleted because it's associated with an Order Item."
                 },
-                status=status.HTTP_405_METHOD_NOT_ALLOWED,
+                status=status.HTTP_403_FORBIDDEN,
             )
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class CollectionList(ListCreateAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(product_count=Count("product"))
     serializer_class = CollectionSerializer
 
     def get_serializer_context(self):
         return {"request": self.request}
-
-
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.annotate(product_count=Count("product"))
-    serializer_class = CollectionSerializer
 
     def delete(self, request, *args, **kwargs):
         collection = self.get_object()
