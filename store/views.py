417367461ea827bc960b43033b57340a14d56424
Jose Servin <servinjose96@gmail.com>
2024-07-25 19:42:56 -0500

    Add Product PageNumber Pagination

--- store/views.py
+++ store/views.py
@@ -11,7 +11,9 @@ from .models import OrderItem, Product, Collection, Review
 from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
 from .filters import ProductFilter
+from .pagination import StandardResultsSetPagination
 
 
 class ProductViewSet(ModelViewSet):
@@ -19,6 +21,7 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    pagination_class = StandardResultsSetPagination
     search_fields = ["title", "description"]
     ordering_fields = ["unit_price", "last_update"]
 
